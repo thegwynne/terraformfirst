7fd0b23b7bdb26496ce629abe842525dc101dcd1
Matthew Gwynne <thegwynne@gmail.com>
2018-08-22 11:52:24 +0100

    after first run

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-	 credentials = "$(file("~/Dropbox/Nimbus/qa/week6/steam-bruin-214108-e576ea6803fd.json"))"
-	 project = "zeta-rush-204812"
+	 credentials = "${file("~/Dropbox/Nimbus/qa/week6/steam-bruin-214108-e576ea6803fd.json")}"
+	 project = "steam-bruin-214108"
 	 region = "europe-west2"
 	 }
